bb15e1e87c41abb0b0c1acce5ba943b6646c1c4c
brunofvprieto <121833188+brunofvprieto@users.noreply.github.com>
2026-08-24 10:54:30 -0300

    Adicionar sitemap de notícias ao robots.txt

--- app/robots.js
+++ app/robots.js
@@ -1,6 +1,9 @@
 export default function robots() {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://acertogames.com.br/sitemap.xml",
+    sitemap: [
+      "https://acertogames.com.br/sitemap.xml",
+      "https://acertogames.com.br/news-sitemap.xml",
+    ],
   };
 }
